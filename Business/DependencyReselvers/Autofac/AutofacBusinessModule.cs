d670387a27e0a335daf0335925165271a7562a8f
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-07-30 14:41:00 +0300

    Auth-Rgister IOC-Controller eklendi.

--- Business/DependencyReselvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyReselvers/Autofac/AutofacBusinessModule.cs
@@ -7,8 +7,10 @@ using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Http;
 
 namespace Business.DependencyReselvers.Autofac
 {
@@ -44,6 +46,14 @@ namespace Business.DependencyReselvers.Autofac
 
             builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
             builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+            builder.RegisterType<HttpContextAccessor>().As<IHttpContextAccessor>();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
